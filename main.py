acbcfea4e721423bc5e7cc74e42155a7ce2d1332
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2026-05-13 21:05:39 +0100

    feat: update main function to run uvicorn server

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello from trueid-backend!")
+from TrueID_BE.api import app
+
+
+def main() -> None:
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
