c92e8ecff125da8daf37c11ab5ed3d7fcc09fcbd
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2026-06-16 10:15:43 +0100

    feat: add admin page to TrueID API Docs

--- TrueID_BE/TrueID_BE.py
+++ TrueID_BE/TrueID_BE.py
@@ -1,6 +1,7 @@
 import reflex as rx
 
 from TrueID_BE.api import app as fastapi_app
+from TrueID_BE.pages.admin import admin_page
 
 
 def code_panel(title: str, body: str) -> rx.Component:
@@ -283,3 +284,4 @@ def index() -> rx.Component:
 
 app = rx.App(api_transformer=fastapi_app)
 app.add_page(index, title="TrueID API Docs")
+app.add_page(admin_page, route="/admin", title="TrueID Admin")
